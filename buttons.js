4fc827b0cf09f7ff32022a74a36608045b67e9dc
chickenfoot88 <chikenfoot88@gmail.com>
2025-10-20 12:31:46 -0300

    add getter setter reactivity like in vue 2

--- buttons.js
+++ buttons.js
@@ -2,40 +2,38 @@ const counterButton = document.querySelector('button#counter')
 const resetButton = document.querySelector('button#reset')
 
 
-function useCounter() {
-  let counter = 5
+const counterState = {
+  _value: 5,
+
+  get value() {
+    return this._value
+  },
+
+  set value(newValue) {
+    this._value = newValue
+    renderCounter()
+  },
   
-  return {
-    getCounter() {
-      return counter
-    },
-    setCounter(value) {
-      counter = value
-      renderCounter()
-    },
-    isCounterTooBig() {
-      return counter > 10
-    }
+  isCounterTooBig() {
+    return this._value > 10
   }
 }
 
-const { getCounter, setCounter, isCounterTooBig } = useCounter()
-
 function renderCounter() {
-  counterButton.textContent = `счетчик: ${getCounter()}`
-  counterButton.classList.toggle('red', isCounterTooBig())
+  counterButton.textContent = `счетчик: ${counterState.value}`
+  counterButton.classList.toggle('red', counterState.isCounterTooBig())
 }
 
 setInterval(() => {
-  setCounter(getCounter() + 1)
+  counterState.value += 1
 }, 1000)
 
 counterButton.addEventListener('click', () => {
-  setCounter(getCounter() + 1)
+  counterState.value += 1
 })
 
 resetButton.addEventListener('click', () => {
-  setCounter(0)
+  counterState.value = 0
 })
 
 renderCounter()
